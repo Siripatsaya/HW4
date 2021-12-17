eadcd2b5fc955ba49295406df874cef97b99a53c
Siripatsaya <siripatsaya.d@mail.kmutt.ac.th>
2021-12-17 14:45:11 +0700

    Create HW4.cs
    HW4

---
+++ HW4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_city;
+            string text1, text2, text3, text4, text5, text6;
+            number_city = int.Parse(Console.ReadLine());
+            text1 = Console.ReadLine();
+            text2 = Console.ReadLine();
+            text3 = Console.ReadLine();
+            text4= Console.ReadLine();
+            text5= Console.ReadLine();
+            text6= Console.ReadLine();
+        }
+    }
+}
